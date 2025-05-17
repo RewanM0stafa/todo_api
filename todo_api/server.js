277b831ebdb260c315f779edabffaeb5b1e80c1e
RewanM0stafa <187568401+RewanM0stafa@users.noreply.github.com>
2025-05-18 02:45:32 +0300

    paths changed

--- todo_api/server.js
+++ todo_api/server.js
@@ -17,7 +17,7 @@ const todoRoutes = require('./routes');
 app.use('/', todoRoutes);
 
 
-const port = process.env.PORT || 3000;
+const port =  3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
